0fe92d67bd395ff423a4b72fb8bbfb9ebfe81ada
shoryataneja <shoryataneja5@gmail.com>
2026-03-14 23:38:38 +0530

    Made the redux setup

--- frontend/src/redux/taskSlice.js
+++ frontend/src/redux/taskSlice.js
@@ -2,8 +2,22 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const taskSlice = createSlice({
   name: 'tasks',
-  initialState: [],
-  reducers: {},
+  initialState: {
+    todo: [],
+    inProgress: [],
+    done: [],
+  },
+  reducers: {
+    addTask: (state, action) => {
+      state.todo.push(action.payload)
+    },
+    moveTask: (state, action) => {
+      const { task, from, to } = action.payload
+      state[from] = state[from].filter((t) => t.id !== task.id)
+      state[to].push(task)
+    },
+  },
 })
 
+export const { addTask, moveTask } = taskSlice.actions
 export default taskSlice.reducer
