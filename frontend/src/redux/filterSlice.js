3e74c4439123ca325d57f53590fea1e8d4b1a7df
shoryataneja <shoryataneja5@gmail.com>
2026-03-15 03:34:52 +0530

    feat: add redux state for task filtering

---
+++ frontend/src/redux/filterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: { filter: 'all' },
+  reducers: {
+    setFilter: (state, action) => {
+      state.filter = action.payload
+    },
+  },
+})
+
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from './taskSlice'
+import filterReducer from './filterSlice'
 import { loadState, saveState } from '../utils/localStorage'
 
 const store = configureStore({
   reducer: {
     tasks: taskReducer,
+    filter: filterReducer,
   },
   preloadedState: loadState('tasks')
     ? { tasks: loadState('tasks') }
