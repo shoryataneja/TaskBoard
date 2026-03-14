9902238312f8d53683603f631e9a4fcb53294505
shoryataneja <shoryataneja5@gmail.com>
2026-03-15 02:36:32 +0530

    fix: add tasks to correct column based on status

--- frontend/src/redux/taskSlice.js
+++ frontend/src/redux/taskSlice.js
@@ -3,13 +3,80 @@ import { createSlice } from '@reduxjs/toolkit'
 const taskSlice = createSlice({
   name: 'tasks',
   initialState: {
-    todo: [],
-    inProgress: [],
-    done: [],
+    todo: [
+      {
+        id: 1,
+        priority: 'Low',
+        title: 'Brainstorming',
+        description: 'Brainstorming brings team members together to generate creative ideas.',
+        avatars: ['AK', 'SR', 'MV'],
+        comments: 12,
+        files: 0,
+      },
+      {
+        id: 2,
+        priority: 'High',
+        title: 'Research',
+        description: 'User research helps you to create an optimal product for users.',
+        avatars: ['PJ', 'RD'],
+        comments: 10,
+        files: 3,
+      },
+      {
+        id: 3,
+        priority: 'High',
+        title: 'Wireframes',
+        description: null,
+        avatars: ['AK', 'MV', 'SR', 'PJ'],
+        comments: 8,
+        files: 2,
+      },
+    ],
+    inProgress: [
+      {
+        id: 4,
+        priority: 'Low',
+        title: 'Onboarding Illustrations',
+        description: null,
+        avatars: ['SR', 'PJ'],
+        comments: 14,
+        files: 15,
+      },
+      {
+        id: 5,
+        priority: 'Low',
+        title: 'Moodboard',
+        description: null,
+        avatars: ['AK', 'RD', 'MV'],
+        comments: 9,
+        files: 10,
+      },
+    ],
+    done: [
+      {
+        id: 6,
+        priority: 'Completed',
+        title: 'Design System',
+        description: 'It just needs to adapt the UI from what you did before.',
+        avatars: ['PJ', 'AK'],
+        comments: 12,
+        files: 15,
+      },
+      {
+        id: 7,
+        priority: 'Completed',
+        title: 'Sticky Notes',
+        description: "We need to make it clear what's important.",
+        avatars: ['SR', 'MV', 'RD'],
+        comments: 9,
+        files: 4,
+      },
+    ],
   },
   reducers: {
     addTask: (state, action) => {
-      state.todo.push(action.payload)
+      const { status } = action.payload
+      state[status].push(action.payload)
     },
     moveTask: (state, action) => {
       const { task, from, to } = action.payload
