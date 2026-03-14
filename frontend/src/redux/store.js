b94afe4f38dc23d9dda0681955f5fd32e1fa943d
shoryataneja <shoryataneja5@gmail.com>
2026-03-15 02:36:24 +0530

    feat: persist task state using localStorage

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from './taskSlice'
+import { loadState, saveState } from '../utils/localStorage'
 
 const store = configureStore({
   reducer: {
     tasks: taskReducer,
   },
+  preloadedState: loadState('tasks')
+    ? { tasks: loadState('tasks') }
+    : undefined,
+})
+
+store.subscribe(() => {
+  saveState('tasks', store.getState().tasks)
 })
 
 export default store
